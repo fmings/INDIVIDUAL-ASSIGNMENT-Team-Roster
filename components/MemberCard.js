e6ffdcf99efa8906b5b2bef813b22f0e7f29ffab
fmings <mingsfelicia@gmail.com>
2024-04-14 14:30:55 -0500

    created membercard component

--- components/MemberCard.js
+++ components/MemberCard.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { Button, Card } from 'react-bootstrap';
+import PropTypes from 'prop-types';
 
-export default function MemberCard() {
+export default function MemberCard({ memberObj }) {
   return (
-    <div>
-      <h1>Member Cards</h1>
-    </div>
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>{memberObj.name}</Card.Title>
+        <Card.Text>{memberObj.role}</Card.Text>
+        <Button variant="secondary">Edit</Button>
+        <Button variant="danger">Delete</Button>
+      </Card.Body>
+    </Card>
   );
 }
+
+MemberCard.propTypes = {
+  memberObj: PropTypes.shape({
+    name: PropTypes.string,
+    role: PropTypes.string,
+    image: PropTypes.string,
+  }).isRequired,
+};
