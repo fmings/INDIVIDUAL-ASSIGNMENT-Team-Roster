fdfe1c4c9ca538af615f1741a4cb0741160e9c99
fmings <mingsfelicia@gmail.com>
2024-04-14 13:42:24 -0500

    created api calls:

---
+++ api/memberData.js
@@ -0,0 +1,80 @@
+import { clientCredentials } from '../utils/client';
+
+const endpoint = clientCredentials.databaseURL;
+
+// API CALL TO GET ALL TEAM MEMBERS
+const getMembers = (uid) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/members.json?orderBy="uid"&equalTo=${uid}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data) {
+        resolve(Object.values(data));
+      } else {
+        resolve([]);
+      }
+    })
+    .catch(reject);
+});
+
+// API CALL TO CREATE NEW TEAM MEMBER
+const createMember = (payload) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/members.json`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+// API CALL TO CREATE GET SINGLE TEAM MEMBER BY FIREBASEKEY
+const getSingleMember = (firebaseKey) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/authors/${firebaseKey}.json`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+// API CALL TO DELETE TEAM MEMBER
+const deleteSingleMember = (firebaseKey) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/authors/${firebaseKey}.json`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+// API CALL TO UPDATE SINGLE TEAM MEMBER
+const updateMember = (payload) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/members/${payload.firebaseKey}.json`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+export {
+  getMembers, getSingleMember, createMember, deleteSingleMember, updateMember,
+};
