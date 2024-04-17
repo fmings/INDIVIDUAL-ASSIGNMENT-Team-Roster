15bb68a82b882bab1c7360856d95b81d3c544c77
fmings <mingsfelicia@gmail.com>
2024-04-17 18:30:22 -0500

    added teamcard component and view function

--- components/TeamCard.js
+++ components/TeamCard.js
@@ -1,9 +1,33 @@
+import Link from 'next/link';
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Button, Card } from 'react-bootstrap';
 
-export default function TeamCard() {
+export default function TeamCard({ teamObj }) {
   return (
-    <div>
-      <h1>Team Card Component</h1>
-    </div>
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={teamObj.logo} />
+      <Card.Body>
+        <Card.Title>{teamObj.team_name}</Card.Title>
+        <Card.Text>{teamObj.city}, {teamObj.state}</Card.Text>
+        <Link href={`/team/edit/${teamObj.firebaseKey}`} passHref>
+          <Button variant="primary">View</Button>
+        </Link>
+        <Link href={`/team/edit/${teamObj.firebaseKey}`} passHref>
+          <Button variant="secondary">Edit</Button>
+        </Link>
+        <Button variant="danger">Delete</Button>
+      </Card.Body>
+    </Card>
   );
 }
+
+TeamCard.propTypes = {
+  teamObj: PropTypes.shape({
+    team_name: PropTypes.string,
+    logo: PropTypes.string,
+    firebaseKey: PropTypes.string,
+    city: PropTypes.string,
+    state: PropTypes.string,
+  }).isRequired,
+};
