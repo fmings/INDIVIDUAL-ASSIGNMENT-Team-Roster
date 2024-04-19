6bd779b6af72f33787adc711628ee19d17b584d3
fmings <mingsfelicia@gmail.com>
2024-04-18 22:40:21 -0500

    removed toggle button from team card

--- components/TeamCard.js
+++ components/TeamCard.js
@@ -3,19 +3,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Button, Card } from 'react-bootstrap';
 import { deleteTeamsAndMembers } from '../api/mergedData';
-import { updateTeam } from '../api/teamData';
 import { useAuth } from '../utils/context/authContext';
 
 export default function TeamCard({ teamObj, onUpdate }) {
   const { user } = useAuth();
 
-  const togglePublic = () => {
-    if (teamObj.public) {
-      updateTeam({ ...teamObj, public: false }).then(() => onUpdate());
-    } else {
-      updateTeam({ ...teamObj, public: true }).then(() => onUpdate());
-    }
-  };
+  // const togglePublic = () => {
+  //   if (teamObj.public) {
+  //     updateTeam({ ...teamObj, public: false }).then(() => onUpdate());
+  //   } else {
+  //     updateTeam({ ...teamObj, public: true }).then(() => onUpdate());
+  //   }
+  // };
 
   const deleteThisTeam = () => {
     if (window.confirm(`Are you sure you want to delete Team ${teamObj.team_name} and all of it's members?`)) {
@@ -28,7 +27,7 @@ export default function TeamCard({ teamObj, onUpdate }) {
       <Card.Img className="card-image" variant="top" src={teamObj.logo} />
       <Card.Body>
         <Card.Title>{teamObj.team_name}</Card.Title>
-        <Button variant="light" className="m-2" onClick={togglePublic}>{teamObj.public ? '🌎' : '🔒'}</Button>
+        <p>{teamObj.public ? '🌎' : '🔒'}</p>
         <Card.Text>{teamObj.city}, {teamObj.state}</Card.Text>
         <Link href={`/team/${teamObj.firebaseKey}`} passHref>
           <Button variant="dark">View</Button>
