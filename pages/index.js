eef226b6451b6c4cec1b7d45199944b6fc54d85e
fmings <mingsfelicia@gmail.com>
2024-04-20 09:10:22 -0500

    updated sign in logo

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,41 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useAuth } from '../utils/context/authContext';
+import { getPublicTeams, getTeams } from '../api/teamData';
 import TeamCard from '../components/TeamCard';
-import { getTeams } from '../api/teamData';
 
 export default function Home() {
-  const [teams, setTeams] = useState([]);
+  const [publicTeams, setPublicTeams] = useState([]);
+  const [privateTeams, setPrivateTeams] = useState([]);
 
   const { user } = useAuth();
 
-  const getAllTeams = () => {
-    getTeams(user.uid).then(setTeams);
+  const getAllPublicTeams = () => {
+    getPublicTeams().then(setPublicTeams);
+  };
+
+  const getAllPrivateTeams = () => {
+    getTeams(user.uid).then(setPrivateTeams);
   };
 
   useEffect(() => {
-    getAllTeams();
+    getAllPrivateTeams();
+    getAllPublicTeams();
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
     <>
       <h1>All Teams</h1>
+      <h3>Public Teams</h3>
       <div className="d-flex flex-wrap">
-        {teams.map((team) => (
-          <TeamCard teamObj={team} key={team.firebaseKey} onUpdate={getAllTeams} />
+        {publicTeams.map((team) => (
+          <TeamCard teamObj={team} key={team.firebaseKey} onUpdate={getAllPublicTeams} />
+        ))}
+      </div>
+      <h3>My Teams</h3>
+      <div className="d-flex flex-wrap">
+        {privateTeams.map((team) => (
+          <TeamCard teamObj={team} key={team.firebaseKey} onUpdate={getAllPrivateTeams} />
         ))}
       </div>
     </>
